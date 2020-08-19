96a62ca365ab6f8a39b54ae19c4d21a260f43afe
Christian Mahnke <cmahnke@gmail.com>
2020-08-19 10:03:00 +0200

    Generate images

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+convert -resize 640x480 "Source Files/Logo/signature-web.png" static/images/logo.png
+
+# Favicons
+# See https://gist.github.com/pfig/1808188
+convert "Source Files/Logo/signature-web.png" -resize 256x256 -transparent white static/images/favicon-256.png
+convert static/images/favicon-256.png -resize 16x16 static/images/favicon-16.png
+convert static/images/favicon-256.png -resize 32x32 static/images/favicon-32.png
+convert static/images/favicon-256.png -resize 64x64 static/images/favicon-64.png
+convert static/images/favicon-256.png -resize 128x128 static/images/favicon-128.png
+convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png static/images/favicon-256.png -colors 256 static/images/favicon.ico
