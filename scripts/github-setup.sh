8bbeed7a64abf62d2e472620b098ad1adbfc6361
Christian Mahnke <cmahnke@gmail.com>
2024-10-30 12:03:13 +0100

    Add Dart-Sass setup

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -4,3 +4,4 @@ CTX_PATH="$(dirname $(realpath $0))"
 
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
